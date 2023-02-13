47080620ef0bd3b39b5cd9f7b6c2ffad60498037
Hakan Kayra Doğan <hkayrad@gmail.com>
2023-02-13 19:12:17 +0300

    compressed video size

--- src/components/BackgroundVideo.tsx
+++ src/components/BackgroundVideo.tsx
@@ -16,7 +16,7 @@ export default function BackgroundVideo() {
 				id="video-id"
 				className="video"
 			>
-				<source src={'/bg/bgVid.mp4'} type="video/mp4" />
+				<source src={'/bg/bg.mp4'} type="video/mp4" />
 				Your browser does not support the video tag.
 			</video>
 		</div>
