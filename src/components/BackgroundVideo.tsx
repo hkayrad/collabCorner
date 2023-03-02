c379f3494e54bb4deeca5b3f6d2bf8b3a1b4ae02
Hakan Kayra Doğan <hkayrad@gmail.com>
2023-03-02 13:06:29 +0300

    changed video format from mp4 to webm

--- src/components/BackgroundVideo.tsx
+++ src/components/BackgroundVideo.tsx
@@ -16,7 +16,7 @@ export default function BackgroundVideo() {
 				id="video-id"
 				className="video"
 			>
-				<source src={'/bg/bg.mp4'} type="video/mp4" />
+				<source src={'/bg/bg.webm'} type="video/webm" />
 				Your browser does not support the video tag.
 			</video>
 		</div>
